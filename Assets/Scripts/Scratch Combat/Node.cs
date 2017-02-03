a7c2cfb39e344bcd684b03f2033cd3b9e5576825
fireteam1013 <emurabata@gmail.com>
2017-02-02 23:55:22 -0800

    New Update
    Again

---
+++ Assets/Scripts/Scratch Combat/Node.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using UnityEngine;
+
+/// <summary>==================================================
+/// Description Here
+/// </summary>=================================================
+
+public class Node : MonoBehaviour
+{
+    Vector2 position;
+    int movementCost;
+    bool canMove;
+
+    public Vector2 Position
+    {
+        get
+        {
+            return position;
+        }
+
+        set
+        {
+            position = value;
+        }
+    }
+
+    public int MovementCost
+    {
+        get
+        {
+            return movementCost;
+        }
+
+        set
+        {
+            movementCost = value;
+        }
+    }
+
+    public bool CanMove
+    {
+        get
+        {
+            return canMove;
+        }
+
+        set
+        {
+            canMove = value;
+        }
+    }
+}
---
+++ Assets/Scripts/Scratch Combat/NodeHolder.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+/// <summary>==================================================
+/// Description Here
+/// </summary>=================================================
+
+public class NodeHolder : MonoBehaviour
+{
+    public Node[,] nodeArray;
+    int mapSizeX;
+    int mapSizeY;
+
+
+    void Start()
+    {
+        nodeArray = new Node[mapSizeX, mapSizeY];   
+    }
+}
