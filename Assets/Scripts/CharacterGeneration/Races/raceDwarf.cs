6f01b7847dad392eb059bf201a55917eeacb6176
fireteam1013 <emurabata@gmail.com>
2017-02-13 16:32:09 -0800

    Race Update
    Modified the races class in prep for finishing it...

--- Assets/Scripts/CharacterGeneration/Races/raceDwarf.cs
+++ Assets/Scripts/CharacterGeneration/Races/raceDwarf.cs
@@ -5,16 +5,43 @@ using System.Collections;
 /// Insert description here
 /// </summary>
 
-public class raceDwarf : MonoBehaviour
+public class raceDwarf : baseRace
 {
-
-
-    //==================================================
-    void Start()
-    //==================================================
+    public raceDwarf()
     {
+        RaceName = "Dwarf";
+        RaceDescription = "Dwarf Description";
 
+        BaseHealth = 6;
+        MinHealthInc = 1;
+        MaxHealthInc = 4;
+
+        BaseMagic = 2;
+        MinMagicInc = 0;
+        MaxMagicInc = 2;
+
+        BaseStrength = 6;
+        MinStrengthInc = 2;
+        MaxStrengthInc = 4;
+
+        BaseAgility = 2;
+        MinAgilityInc = 0;
+        MaxAgilityInc = 3;
+
+        BaseIntelligence = 2;
+        MinIntelligenceInc = 0;
+        MaxIntelligenceInc = 2;
+
+        BaseResistance = 6;
+        MinResistanceInc = 2;
+        MaxResistanceInc = 4;
+
+        BaseReflex = 2;
+        MinReflexInc = 0;
+        MaxReflexInc = 2;
+
+        BaseWill = 6;
+        MinWillInc = 2;
+        MaxWillInc = 4;
     }
-
-
 }
